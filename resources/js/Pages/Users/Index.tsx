f4ba2c79b68ce487a411cefe8cbfadc0ad88275b
Rich Perez <imperez5@gmail.com>
2025-04-05 13:35:47 -0400

    don't allow editing if the user is deactivtaed.

--- resources/js/Pages/Users/Index.tsx
+++ resources/js/Pages/Users/Index.tsx
@@ -26,6 +26,7 @@ export default function Dashboard({ auth, users }: ListProps) {
                         <th>Email</th>
                         <th>Last Login</th>
                         <th>Deactivated</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,9 +38,12 @@ export default function Dashboard({ auth, users }: ListProps) {
                             <td>{shortFormat(user.last_login_at)}</td>
                             <td>{shortFormat(user.deactivated_at)}</td>
                             <td className="flex gap-2">
-                                <Link href={route('users.edit', [user.id])} className="btn btn-xs btn-primary">
-                                    Edit
-                                </Link>
+                                {user.deactivated_at
+                                    ? null
+                                    : <Link href={route('users.edit', [user.id])} className="btn btn-xs btn-primary">
+                                        Edit
+                                    </Link>
+                                }
                                 {user.deactivated_at
                                     ? <Link href={route('users.restore', [user.id])} className="btn btn-xs btn-link text-green-600" method="post" as="button">
                                         Activate
